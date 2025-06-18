14cebe216c9c0e901e961958e1b9983967fd7f00
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2025-06-18 12:06:48 +0100

    downloading data (not ssrd

--- download_data/submit_download_data.bash
+++ download_data/submit_download_data.bash
@@ -5,14 +5,14 @@
 #SBATCH --account=canari
 #SBATCH --partition=standard
 #SBATCH --qos=long
-#SBATCH -o /home/users/benhutch/unseen_multi_year/logs/submit_download_ERA5-%A_%a.out
-#SBATCH -e /home/users/benhutch/unseen_multi_year/logs/submit_download_ERA5-%A_%a.err
+#SBATCH -o /home/users/benhutch/LDS_UNSEEN/logs/submit_download_ERA5-%A_%a.out
+#SBATCH -e /home/users/benhutch/LDS_UNSEEN/logs/submit_download_ERA5-%A_%a.err
 
 # Set up the usage message
-usage="Usage: sbatch submit_download_ERA5_jasmin.bash <year>"
+usage="Usage: sbatch submit_download_ERA5_jasmin.bash <start_year> <end_year> <start_month> <end_month>"
 
 # Check the number of CLI arguments
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 4 ]; then
     echo "Illegal number of parameters"
     echo $usage
     exit 1
@@ -24,28 +24,34 @@ module load jaspy
 # Load my python environment
 source activate bens-conda-env2
 
-# Set up the months
-months=(1 2 12)
-
-# Set up the year
-year=$1
+# Load the args
+start_year=$1
+end_year=$2
+start_month=$3
+end_month=$4
 
 # Set up the process script
-process_script="/home/users/benhutch/unseen_multi_year/downloading_data/download_ERA5_jasmin.py"
+process_script="/home/users/benhutch/LDS_UNSEEN/download_data/download_ERA5_jasmin.py"
 
-# Echo the year arg
-echo "Year: ${year}"
+# Echo the args
+echo "Start year: ${start_year}"
+echo "End year: ${end_year}"
+echo "Start month: ${start_month}"
+echo "End month: ${end_month}"
 
-# Loop over the months
-for month in "${months[@]}"; do
-    # Echo the month number
-    echo "Month no: ${month}"
+# ensure that these are all ints
+if ! [[ "$start_year" =~ ^[0-9]+$ ]] || ! [[ "$end_year" =~ ^[0-9]+$ ]] || ! [[ "$start_month" =~ ^[0-9]+$ ]] || ! [[ "$end_month" =~ ^[0-9]+$ ]]; then
+    echo "All arguments must be integers."
+    echo $usage
+    exit 1
+fi
 
-    # Run the script
-    python ${process_script} \
-        --year ${year} \
-        --month ${month}
-done
+# run the python script with the arguments
+python ${process_script} \
+    --start_year ${start_year} \
+    --end_year ${end_year} \
+    --start_month ${start_month} \
+    --end_month ${end_month}
 
 # End of file
 echo "End of file"
